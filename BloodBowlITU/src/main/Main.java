0f1c69a81aaf6ae0358a24886ae0f825796b44c1
Jacob Grønlund Dinesen <jacobgdinesen@gmail.com>
2013-01-30 16:11:28 +0100

    renderer and gameloop

--- BloodBowlITU/src/main/Main.java
+++ BloodBowlITU/src/main/Main.java
@@ -1,16 +1,52 @@
 package main;
 
+import java.util.Date;
+
+import game.GameMaster;
 import test.DiceTester;
+import view.InputManager;
+import view.Renderer;
 
 public class Main {
 
+	private static InputManager inputManager;
+	private static Renderer renderer;
+	private static GameMaster gameMaster;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
 		DiceTester.testDices();
-		
+		initialize();
+		startGame();
 	}
 
+	public static void initialize(){
+		inputManager = new InputManager();
+		gameMaster = new GameMaster();
+		renderer = new Renderer(60, gameMaster, inputManager);
+	}
+	
+	public static void startGame(){
+
+
+		
+		long startTime = new Date().getTime();
+		
+		while(true){
+			startTime = new Date().getTime();
+			
+			gameMaster.update();
+			renderer.renderFrame();
+			
+			long delta = new Date().getTime() - startTime;
+			try {
+				Thread.sleep(1000/renderer.getFps() - delta);
+			} catch (InterruptedException e) {e.printStackTrace();}
+			
+		}
+	
+		
+		}
 }
---
+++ BloodBowlITU/src/view/Renderer.java
@@ -0,0 +1,56 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import game.GameMaster;
+
+public class Renderer extends JPanel{
+
+	private int fps;
+	private GameMaster gameMaster;
+	
+	private static JFrame screen;
+	private static int screenWidth = 844;
+	private static int screenHeight = 598;
+	private static InputManager inputManager;
+	
+	public Renderer(int fps, GameMaster gm, InputManager im){
+		this.fps = fps;
+		this.gameMaster = gm;
+		this.inputManager = im;
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		screen = new JFrame();
+		//screen.setSize(screenWidth, screenHeight);
+		
+		screen.setTitle("no coincidance");
+		screen.setVisible(true);
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen.add(this);
+		screen.pack();
+		this.addKeyListener(inputManager);
+		this.addMouseListener(inputManager);
+		this.addMouseMotionListener(inputManager);
+		
+	}
+	
+	public int getFps(){
+		return fps;
+	}
+	
+	public void renderFrame(){
+		System.out.println("RENDERFRAME CALLED");
+		this.repaint();
+	}
+	
+	public void paintComponent(Graphics g) {
+		g.fillRect(2, 2, 840, 40);
+		g.setColor(Color.GREEN);
+		g.fillRect(2, 44, 840, 510);
+		g.setColor(Color.RED);
+		g.fillRect(2, 556, 300, 40);
+		System.out.println("FRAMES RENDERED");
+	}
+}
