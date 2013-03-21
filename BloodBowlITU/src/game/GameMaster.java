d9450cacc07e8bbea51cc096287ab1e10fac8d4c
niels justesen <nju@it-minds.dk>
2013-03-21 14:36:56 +0100

    debug kickoff

--- BloodBowlITU/src/game/GameMaster.java
+++ BloodBowlITU/src/game/GameMaster.java
@@ -3468,9 +3468,8 @@ public class GameMaster {
 		
 		// DEBUGGING
 		//blitz();
-		throwARock();
+		//throwARock();
 		
-		/*
 		switch(roll){
 			case 2: getTheRef(); break;
 			case 3: riot(); break;
@@ -3484,7 +3483,7 @@ public class GameMaster {
 			case 11: throwARock(); break;
 			case 12: pitchInvasion(); break;
 		}
-		*/
+		
 	}
 
 	/**
